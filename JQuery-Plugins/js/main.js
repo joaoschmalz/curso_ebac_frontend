5685508372161b56f4bb42c77a9a8e331d92d7b7
Joao Schmalz <joaom.sch@outlook.com>
2023-01-14 15:53:37 -0300

    innitial commit

---
+++ JQuery-Plugins/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('#cpf').mask('000.000.000-00', {
+        placeholder:'000.000.000-00'
+    });
+
+    $('#cep').mask('00000-000', {
+        placeholder:'00000-000'
+    });
+})
