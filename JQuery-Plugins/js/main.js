7d799d759a7379ec96addcfa29689dcc934a2ccc
Joao Schmalz <joaom.sch@outlook.com>
2023-01-16 07:43:49 -0300

    Ajuste campo telefone

--- JQuery-Plugins/js/main.js
+++ JQuery-Plugins/js/main.js
@@ -3,6 +3,10 @@ $(document).ready(function(){
         placeholder:'000.000.000-00'
     });
 
+    $('#telefone').mask('(00) 0 0000-0000', {
+        placeholder:'(00) 0 0000-0000'
+    });
+
     $('#cep').mask('00000-000', {
         placeholder:'00000-000'
     });
